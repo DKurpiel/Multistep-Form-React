18d577b7db5144641cb7b0066afb3f2d1c91e134
Damian <kurpiel09@gmail.com>
2022-11-15 10:57:26 +0100

    ready react app

---
+++ src/AccountForm.tsx
@@ -0,0 +1,26 @@
+import { FormWrapper } from "./FormWrapper";
+
+type AccountData = {
+  email: string
+  password: string
+
+}
+
+type AccountFormProps = AccountData & {
+  updateFields: (fields: Partial<AccountData>) => void
+}
+
+export function AccountForm({ email, password, updateFields }: AccountFormProps) {
+  return (
+    <FormWrapper title="Account Creation">
+      <label htmlFor="">Email</label>
+      <input type="email" autoFocus required
+        value={email}
+        onChange={e => updateFields({ email: e.target.value })} />
+      <label htmlFor="">Password</label>
+      <input type="password" required
+        value={password}
+        onChange={e => updateFields({ password: e.target.value })} />
+    </FormWrapper>
+  )
+}
